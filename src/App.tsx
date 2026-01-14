4edee0e004ba27f9a498bfec8892b70f060114ae
seangah8 <seangah8@gmail.com>
2026-01-14 07:50:30 +0200

    feat: add ContactSection to App and update styles to include new section, enhancing overall site structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HeadlineSection } from './sections/HeadlineSection'
 import { AboutSection } from './sections/AboutSection'
 import { ProjectsSection } from './sections/ProjectsSection'
+import { ContactSection } from './sections/ContactSection'
 export default function App() {
 
 
@@ -9,6 +10,7 @@ export default function App() {
       <HeadlineSection />
       <AboutSection />
       <ProjectsSection />
+      <ContactSection />
     </>
   )
 }
@@ -17,5 +19,11 @@ export default function App() {
 
 /*
   TODO:
-    
+    1. create a new section for the contact me section
+    2. create a title in the middle of the section with a quastion like "want to make your dream website come true?"
+    4. make the background color change when entering the section
+    4. when clicking the title it will fade
+    5. affter fading, 3 color horisontal lines will come from left, right and left again and in each a bouncy animation will show the media logo
+    6. hovering each line will show the media name and the link to the media
+    7. clicking it will open the media in a new tab
 */
---
+++ src/sections/ContactSection.tsx
@@ -0,0 +1,8 @@
+
+export function ContactSection() {
+    return (
+        <div className="contact-section">
+            <h1>want to make your dream website come true?</h1>
+        </div>
+    )
+}
